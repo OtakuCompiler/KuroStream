9e167c3599cb159c0656ece6f02cf794c9de2115
OtakuCompiler <otakucompiler@users.noreply.github.com>
2026-07-11 21:35:27 +0500

    Add missing buildFeatures{compose=true} to :common
    Library module used @Composable/remember but lacked buildFeatures.compose,
    causing 'couldn't find inline method remember' IR lowering error. Now
    consistent with :app/:playback/:extensions.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -34,6 +34,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions { jvmTarget = "17" }
+    buildFeatures {
+        compose = true
+    }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.4"
     }
