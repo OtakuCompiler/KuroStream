9cb45cf0e16713c7776f98719e2360cf8c6fc188
OtakuCompiler <otakucompiler@users.noreply.github.com>
2026-07-11 13:29:22 +0500

    Fix CI: Add dependencycheck version and update Gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,16 +14,16 @@
 // along with KuroStream.  If not, see <https://www.gnu.org/licenses/>.
 
 plugins {
-    id("com.android.application") version "8.7.0" apply false
-    id("com.android.library") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.diffplug.spotless") version "6.25.0" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
-    id("org.owasp.dependencycheck") version "9.0.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.dependencycheck) apply false
 }
 
 apply(plugin = "org.owasp.dependencycheck")
