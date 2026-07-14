342965735a8d9db46545c7c9e0c5ae93f0cb12f3
root <root@localhost.localdomain>
2026-07-14 17:00:21 +0500

    fix: move Components.kt to jvmMain/androidMain - javax.inject not available in JS target

---
+++ core-common/src/jvmMain/kotlin/com/kurostream/core/common/di/Components.kt
@@ -0,0 +1,32 @@
+// This file is part of KuroStream.
+//
+// KuroStream is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// KuroStream is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with KuroStream.  If not, see <https://www.gnu.org/licenses/>.
+
+package com.kurostream.core.common.di
+
+import com.kurostream.core.common.dispatcher.DispatcherProvider
+import com.kurostream.core.common.dispatcher.DefaultDispatcherProvider
+import javax.inject.Provider
+
+interface DispatcherProviderComponent {
+    val dispatcherProvider: Provider<DispatcherProvider>
+}
+
+interface LoggerComponent {
+    val loggerProvider: Provider<com.kurostream.core.platform.PlatformLogger>
+}
+
+interface PlatformComponent {
+    val platformFactory: Provider<com.kurostream.core.platform.PlatformFactory>
+}
