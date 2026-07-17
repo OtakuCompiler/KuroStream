f68b7d8d91f754b6b76fb6f0789c4208d45db3c9
OtakuCompiler <mystical1309@gmail.com>
2026-07-17 08:56:06 +0500

    fix: remove detekt task disable code from modules
    All modules had 'tasks.named(detekt)' or 'tasks.withType<Detekt>()' code
    that caused spotless check to fail because detekt plugin is no longer applied globally.
    Removed all such code since detekt is not applied globally anymore.

--- cache/build.gradle.kts
+++ cache/build.gradle.kts
@@ -20,11 +20,6 @@ plugins {
     alias(libs.plugins.ksp)
 }
 
-// Disable detekt due to ClassCastException bug in detekt 1.23.x
-tasks.named("detekt") {
-    enabled = false
-}
-
 android {
     namespace = "com.kurostream.cache"
     compileSdk = libs.versions.compileSdk.get().toInt()
