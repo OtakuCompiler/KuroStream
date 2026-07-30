dbbd02a31c2f9d93f21735ccca9cded57ccf34df
anomalyco <anomalyco@github.com>
2026-07-31 04:29:46 +0500

    Remove SeekParameters call

--- playback/src/main/java/com/kurostream/players/media3/Media3Player.kt
+++ playback/src/main/java/com/kurostream/players/media3/Media3Player.kt
@@ -6,7 +6,6 @@ import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import androidx.media3.common.Player
-import androidx.media3.common.SeekParameters
 import androidx.media3.common.util.UnstableApi
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -47,7 +46,6 @@ class Media3Player @Inject constructor(
                     setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON)
                 }
             )
-            .setSeekParameters(SeekParameters.CLOSEST_SYNC)
             .build()
     }
 }
