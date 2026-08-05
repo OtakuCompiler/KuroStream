af1b2d7c95cee0289f834e64ff6e507ed7e19afe
KuroStream Dev <dev@kurostream.app>
2026-08-05 13:12:37 +0000

    fix: add missing TvdbApi and ImdbApi Hilt providers

--- data/src/main/java/com/kurostream/data/di/NetworkModule.kt
+++ data/src/main/java/com/kurostream/data/di/NetworkModule.kt
@@ -22,6 +22,8 @@ import com.kurostream.data.remote.api.KitsuApi
 import com.kurostream.data.remote.api.MalApi
 import com.kurostream.data.remote.api.OpenSubtitlesApi
 import com.kurostream.data.remote.api.TmdbApi
+import com.kurostream.data.remote.api.TvdbApi
+import com.kurostream.data.remote.api.ImdbApi
 import com.kurostream.data.remote.api.YouTubeApi
 import com.kurostream.data.network.security.CertificatePinningConfig
 import dagger.Module
@@ -50,6 +52,8 @@ object NetworkModule {
     private const val KITSU_BASE_URL = "https://kitsu.io/api/edge/"
     private const val OPENSUBTITLES_BASE_URL = "https://api.opensubtitles.com/"
     private const val YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/"
+    private const val TVDB_BASE_URL = "https://api.thetvdb.com/"
+    private const val IMDB_BASE_URL = "https://imdb-api.com/"
 
 
     @Provides
@@ -190,5 +194,27 @@ object NetworkModule {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         return retrofit.create(YouTubeApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTvdbApi(client: OkHttpClient): TvdbApi {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(TVDB_BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(TvdbApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideImdbApi(client: OkHttpClient): ImdbApi {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(IMDB_BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(ImdbApi::class.java)
     }
 }
